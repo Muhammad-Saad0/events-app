d31350a69cf89727e4865d32e6ac468698cb816d
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-05-04 21:36:00 +0500

    created events page(unstyled)

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,42 @@
 import React from "react";
 
-function page() {
+function page({ data }) {
   return (
-    <div>
-      <h1>This is events page</h1>
-    </div>
+    <section>
+      <h1 className="text-4xl font-poppins text-center p-8">
+        This is Events page
+      </h1>
+      <div>
+        {data.map((ev) => {
+          return (
+            <div key={ev.id} className="m-10">
+              <a href={`/events/${ev.id}`}>
+                <h2 className="text-2xl font-poppins">
+                  {ev.title}
+                </h2>
+              </a>
+              <img
+                className="w-[400px] h-[300px]"
+                src={ev.image}
+                alt="city-image"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }
 
 export default page;
+
+export async function getStaticProps(ctx) {
+  const { events_categories } = await import(
+    "../../data/data.json"
+  );
+  return {
+    props: {
+      data: events_categories,
+    },
+  };
+}
